6446af634a9524f53e1535608600a1148e4e41b9
Jorge Haddad <jorge.haddad@sonos.com>
2017-10-14 16:36:45 -0700

    - introduce citizenship - create conqueror - pickup stray minerals

--- dist/GeneralUtils.js
+++ dist/GeneralUtils.js
@@ -1,6 +1,6 @@
 let generalUtils;
 module.exports = generalUtils = {
-    getLowestPathCostEntity: function (reference, collection) {
+    sortByLowestPathCost: function (reference, collection) {
         return collection.sort(function (a, b) {
             aPathCost = PathFinder.search(reference.pos, a.pos).cost;
             bPathCost = PathFinder.search(reference.pos, b.pos).cost;
@@ -11,12 +11,14 @@ module.exports = generalUtils = {
         console.log("**Game Stats**");
         console.log(`Cpu bucket count: ${Game.cpu.bucket}`);
         // print creeps stats
-        console.log(`State: ${state} | guards ${Memory.guards.length}/${Memory.numGuards} | repairmans ${Memory.repairmans.length}/${Memory.numRepairmans} | upgraders  ${Memory.upgraders.length}/${Memory.numUpgraders} | commuter upgraders  ${Memory.commuterUpgraders.length}/${Memory.numCommuterUpgraders} | builders   ${Memory.builders.length}/${Memory.numBuilders} | harvesters ${Memory.harvesters.length}/${Memory.numHarvesters} | commuter harvesters ${Memory.commuterHarvesters.length}/${Memory.numCommuterHarvesters}|`);
+        console.log(`State: ${state} | guards ${Memory.guards.length}/${Memory.numGuards} | repairmans ${Memory.repairmans.length}/${Memory.numRepairmans} | upgraders  ${Memory.upgraders.length}/${Memory.numUpgraders} | commuter upgraders  ${Memory.commuterUpgraders.length}/${Memory.numCommuterUpgraders} | builders   ${Memory.builders.length}/${Memory.numBuilders} | harvesters ${Memory.harvesters.length}/${Memory.numHarvesters} | commuter harvesters ${Memory.commuterHarvesters.length}/${Memory.numCommuterHarvesters}| conqueror ${Memory.conquerors.length}/${Memory.numConquerors}|`);
         // print available rooms info
         Object.keys(Game.rooms).forEach(function (roomName) {
             const room = Game.rooms[roomName];
-            console.log(`Room ${roomName} is available and has:
-            - Energy ${room.energyAvailable}/${room.energyCapacityAvailable}  stored/capacity`)
+            console.log(`Room ${roomName}:
+            - Energy ${room.energyAvailable}/${room.energyCapacityAvailable}  stored/capacity
+            - Citizens: ${_.values(Game.creeps).filter((c) => c.memory.citizenship === roomName).length}`
+            )
         });
     },
     resetStoredStructures: function () {
