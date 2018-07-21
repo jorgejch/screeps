f36d4a4fac5cf5a60cfcbb9e0c015034cc89f5de
Jorge Haddad <jorgejch@gmail.com>
2018-07-20 21:36:06 -0700

    wip

---
+++ src/event.constructionAndRepair.js
@@ -0,0 +1,18 @@
+module.exports = {
+    cons: function (flag) {
+        const source = flag.pos.lookFor(LOOK_SOURCES)[0]
+        const label = `harvest_manager_of_source_${source.id}`
+        const visual = new RoomVisual(flag.roomName)
+        if (Kernel.getProcessByLabel(label)) {
+            visual.text(`Process to harvest source ${source.id} already exists.`, flag.pos)
+            return
+        }
+        const process = Kernel.scheduler.launchProcess(
+            Kernel.availableProcessClasses.SourceHarvestManager,
+            label
+        )
+        visual.text(`Launched process ${process.label}.`, flag.pos)
+        process.sourceId = source.id
+        process.ownerRoomName = source.room.name
+    }
+}
--- src/os.eventFuncMap.js
+++ src/os.eventFuncMap.js
@@ -5,5 +5,6 @@ const controller = require("event.controller")
 module.exports = {
     "1_1" /*RED_RED*/    : test.sayHelloWorld,
     "2_1" /*PURPLE_RED*/ : source.harvestSourceUnderFlagForOwnRoom,
-    "4_1" /*CYAN_RED*/   : controller.upgradeControllerUnderFlag
+    "4_1" /*CYAN_RED*/   : controller.upgradeControllerUnderFlag,
+    "5_1" /*GREEN_RED*/  :
 }
