a3a40a9c62bedb104f59494694428086784c4fd7
Jorge Haddad <jorgejch@gmail.com>
2018-08-05 12:49:34 -0700

    add manager to repair room; make num of builders on construction managers gradated with level as from the repair manager

--- src/process.rooms.js
+++ src/process.rooms.js
@@ -10,14 +10,6 @@ const obtainEnergyOptions = require("util.obtainEnergyOptions")
 
 module.exports = {
     ConstructionManager: class extends mixins.ActivityDirectorProcess(BaseProcess) {
-        set targetRoomName(name) {
-            this.data.targetRoomName = name
-        }
-
-        get targetRoomName() {
-            return this.data.targetRoomName
-        }
-
         run() {
             const role = "builder"
             this.cleanRoleDeadProcesses(role)
@@ -43,41 +35,41 @@ module.exports = {
 
                 const sourceEnergyTaskTicket = processUtils.getEnergySourcingTaskTicket(sourceOption, sourceRoomName)
                 const energyCapacityAvailable = this.ownerRoom.energyCapacityAvailable
-                let bodyType, currentLevel, numOfBuilders
+                let bodyType, currentLevel, numOfCreeps
 
                 if (energyCapacityAvailable < energyCapacityLevels.LEVEL_2) {
                     bodyType = "BASIC_WORKER_1"
                     currentLevel = 1
-                    numOfBuilders = targetRoomConstructionSites.length > 3 ? 3 : 1
+                    numOfCreeps = targetRoomConstructionSites.length > 3 ? 3 : 1
                 }
                 else if (energyCapacityAvailable < energyCapacityLevels.LEVEL_3) {
                     currentLevel = 2
                     this.resolveLevelForRole(role, currentLevel)
                     bodyType = "BASIC_WORKER_2"
-                    numOfBuilders = targetRoomConstructionSites.length > 3 ? 3 : 1
+                    numOfCreeps = 1 + Math.min(Math.trunc(targetRoomConstructionSites.length / 5), 3)
                 }
                 else if (energyCapacityAvailable < energyCapacityLevels.LEVEL_4) {
                     currentLevel = 3
                     this.resolveLevelForRole(role, currentLevel)
                     bodyType = "BASIC_WORKER_3"
-                    numOfBuilders = targetRoomConstructionSites.length > 3 ? 2 : 1
+                    numOfCreeps = 1 + Math.min(Math.trunc(targetRoomConstructionSites.length / 5), 2)
                 }
                 else if (energyCapacityAvailable < energyCapacityLevels.LEVEL_5) {
                     currentLevel = 4
                     this.resolveLevelForRole(role, currentLevel)
                     bodyType = "BASIC_WORKER_4"
-                    numOfBuilders = 1
+                    numOfCreeps = 1 + Math.min(Math.trunc(targetRoomConstructionSites.length / 7), 2)
                 }
                 else {
                     currentLevel = 5
                     this.resolveLevelForRole(role, currentLevel)
                     bodyType = "BASIC_WORKER_5"
-                    numOfBuilders = 1
+                    numOfCreeps = 1 + Math.min(Math.trunc(targetRoomConstructionSites.length / 10), 2)
                 }
 
                 this.resolveRoleProcessesQuantity(
                     role,
-                    numOfBuilders,
+                    numOfCreeps,
                     bodyType,
                     15,
                     [
@@ -427,6 +419,113 @@ module.exports = {
 
             this._printStats()
         }
+    },
+    RepairManager: class extends mixins.ActivityDirectorProcess(BaseProcess) {
+        run() {
+            const role = "repairman"
+            this.cleanRoleDeadProcesses(role)
+
+            if (!this.targetRoom) {
+                console.log(`Process ${this.label} does not visibility into ${this.targetRoomName}.`)
+                return
+            }
+
+            const damagedStructuresAtThreshold = this.targetRoom
+                .find(FIND_STRUCTURES).filter(struct => {
+                    if (struct.hits < struct.hitsMax/3) {
+                        if (struct.structureType === STRUCTURE_WALL) {
+                            return struct.hits < config.MAX_WALL_HITS_LIMIT/3
+                        }
+                        else if (struct.structureType === STRUCTURE_RAMPART) {
+                            return struct.hits < config.MAX_RAMPART_HITS_LIMIT/3
+                        }
+                        else if (struct.structureType === STRUCTURE_CONTAINER) {
+                            // TODO: as a migration right side dividend has to be slowly brought to 1
+                            return struct.hits < struct.hitsMax / 10
+                        }
+                        else {
+                            return true
+                        }
+                    }
+                })
+
+            if (damagedStructuresAtThreshold.length > 0) {
+                const ownerRoomSourceOption = processUtils.determineEnergyObtentionMethod(this.ownerRoom)
+                const targetRoomSourceOption = processUtils.determineEnergyObtentionMethod(this.targetRoom)
+                let sourceOption, sourceRoomName
+
+                // rather than harvest on the target leech from owner
+                if (
+                    targetRoomSourceOption === obtainEnergyOptions.HARVEST
+                    && ownerRoomSourceOption !== obtainEnergyOptions.HARVEST
+                ) {
+                    sourceOption = ownerRoomSourceOption
+                    sourceRoomName = this.ownerRoomName
+                } else {
+                    sourceOption = targetRoomSourceOption
+                    sourceRoomName = this.targetRoomName
+                }
+
+                const sourceEnergyTaskTicket = processUtils.getEnergySourcingTaskTicket(sourceOption, sourceRoomName)
+                const energyCapacityAvailable = this.ownerRoom.energyCapacityAvailable
+                let bodyType, currentLevel, numOfCreeps
+
+                if (energyCapacityAvailable < energyCapacityLevels.LEVEL_2) {
+                    bodyType = "BASIC_WORKER_1"
+                    currentLevel = 1
+                    numOfCreeps = 1 + Math.min(Math.trunc(damagedStructuresAtThreshold.length / 5), 3)
+                }
+                else if (energyCapacityAvailable < energyCapacityLevels.LEVEL_3) {
+                    currentLevel = 2
+                    this.resolveLevelForRole(role, currentLevel)
+                    bodyType = "BASIC_WORKER_2"
+                    numOfCreeps = 1 + Math.min(Math.trunc(damagedStructuresAtThreshold.length / 5), 2)
+                }
+                else if (energyCapacityAvailable < energyCapacityLevels.LEVEL_4) {
+                    currentLevel = 3
+                    this.resolveLevelForRole(role, currentLevel)
+                    bodyType = "BASIC_WORKER_3"
+                    numOfCreeps = 1 + Math.min(Math.trunc(damagedStructuresAtThreshold.length / 5), 1)
+                }
+                else if (energyCapacityAvailable < energyCapacityLevels.LEVEL_5) {
+                    currentLevel = 4
+                    this.resolveLevelForRole(role, currentLevel)
+                    bodyType = "BASIC_WORKER_4"
+                    numOfCreeps = 1
+                }
+                else {
+                    currentLevel = 5
+                    this.resolveLevelForRole(role, currentLevel)
+                    bodyType = "BASIC_WORKER_5"
+                    numOfCreeps = 1
+                }
+
+                this.resolveRoleProcessesQuantity(
+                    role,
+                    numOfCreeps,
+                    bodyType,
+                    10,
+                    [
+                        new tasks.TaskTicket(
+                            tasks.tasks.CYCLIC_PICKUP_DROPPED_RESOURCE_ON_ROOM.name,
+                            {roomName: this.targetRoomName}
+                        ),
+                        sourceEnergyTaskTicket,
+                        new tasks.TaskTicket(
+                            tasks.tasks.CYCLIC_REPAIR_ROOM_STRUCTURES.name, {roomName: this.targetRoomName}
+                        )
+                    ],
+                    this.targetRoomName,
+                    currentLevel
+                )
+            }
+            else {
+                if (this._getRoleProcessesLabels(role).length > 0) {
+                    this.setAllRoleProcessesToDieAfterCreep(role)
+                }
+            }
+        }
+
     },
     TowerManager: class extends BaseProcess {
         set towerId(id) {
