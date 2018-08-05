7acd7c11788c0f82e8b412362c602acbd7cddcaf
Jorge Haddad <jorgejch@gmail.com>
2018-08-05 12:20:10 -0700

    add utility event to delete a room's repair manager

---
+++ src/event.utility.js
@@ -0,0 +1,8 @@
+module.exports = {
+    deleteRoomRepairManager: (flag) => {
+        const targetRoomName = flag.pos.roomName
+        const ownerRoomName = flag.name
+        const process = Kernel.getProcessByLabel(`repair_manager_of_room_${targetRoomName}_from_${ownerRoomName}`)
+        process.die()
+    }
+}
