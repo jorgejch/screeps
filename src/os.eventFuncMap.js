6631af5cc03aaa97704595b2558c372c3b1badc9
Jorge Haddad <jorgejch@gmail.com>
2018-08-05 12:21:17 -0700

    register repair and utility event

--- src/os.eventFuncMap.js
+++ src/os.eventFuncMap.js
@@ -1,8 +1,9 @@
 const test = require("event.test")
 const source = require("event.source")
 const controller = require("event.controller")
-const constructionAnbRepair = require("event.constructionAndRepair")
+const constructionAndRepair = require("event.constructionAndRepair")
 const foreignRelations = require("event.foreignRelations")
+const utility = require("event.utility")
 
 module.exports = {
     "1_1" /*RED_RED*/    : test.sayHelloWorld,
@@ -10,7 +11,9 @@ module.exports = {
     "2_2" /*PURPLE_PURPLE*/ : source.harvestSourceUnderFlagOnRemoteRoom,
     "4_1" /*CYAN_RED*/   : controller.upgradeControllerUnderFlagFromOwnRoom,
     "4_2" /*CYAN_PURPLE*/   : controller.reserveControllerUnderFlag,
-    "5_1" /*GREEN_RED*/  : constructionAnbRepair.constructRemoteRoomUnderFlag,
+    "5_1" /*GREEN_RED*/  : constructionAndRepair.constructRemoteRoomUnderFlag,
+    "5_2" /*GREEN_PURPLE*/  : constructionAndRepair.repairRemoteRoomUnderFlag,
     "6_1" /*YELLOW_RED*/ : foreignRelations.sendScoutToFlagPosition,
-    "6_2" /*YELLOW_PURPLE*/ : foreignRelations.guardRoomUnderFlag
+    "6_2" /*YELLOW_PURPLE*/ : foreignRelations.guardRoomUnderFlag,
+    "9_1" /*GREY_RED*/ : utility.deleteRoomRepairManager
 }
