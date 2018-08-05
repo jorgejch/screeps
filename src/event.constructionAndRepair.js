dffb5b46e230c1a9e36c6c853bf4898a1ee73683
Jorge Haddad <jorgejch@gmail.com>
2018-08-05 12:05:45 -0700

    add event to create repair manager for room

--- src/event.constructionAndRepair.js
+++ src/event.constructionAndRepair.js
@@ -22,5 +22,27 @@ module.exports = {
         visual.text(`Launched process ${process.label}.`, flag.pos)
         process.ownerRoomName = ownerRoomName
         process.targetRoomName = flag.room.name
+    },
+
+    repairRemoteRoomUnderFlag: function (flag) {
+        const visual = new RoomVisual(flag.room.name)
+        const targetRoom = flag.room
+        const ownerRoomName = flag.name
+        if (!processUtils.checkRoomExists(ownerRoomName)) {
+            visual.text(`Room ${ownerRoomName} doesn't exist.`, flag.pos)
+            return
+        }
+        const label = `repair_manager_of_room_${targetRoom.name}_from_${ownerRoomName}`
+        if (Kernel.getProcessByLabel(label)) {
+            visual.text(`Process to repair room ${targetRoom.name} already exists.`, flag.pos)
+            return
+        }
+        const process = Kernel.scheduler.launchProcess(
+            Kernel.availableProcessClasses.RepairManager,
+            label
+        )
+        visual.text(`Launched process ${process.label}.`, flag.pos)
+        process.ownerRoomName = ownerRoomName
+        process.targetRoomName = flag.room.name
     }
 }
